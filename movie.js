eb24b827e3374349b6fb7a0c8b1df78d177fe165
Jaya <jaya.kolte21@gmail.com>
2025-06-17 14:22:11 -0500

    Practicing creating JSON object

---
+++ movie.js
@@ -0,0 +1,16 @@
+{
+  "title": "The Great Adventure",
+  "year": 2022,
+  "isAvailable": true,
+  "genres": ["Action", "Adventure", "Thriller"],
+  "cast": [
+    {
+      "name": "Emma Stone",
+      "role": "Lead"
+    },
+    {
+      "name": "John Boyega",
+      "role": "Supporting"
+    }
+  ]
+}
